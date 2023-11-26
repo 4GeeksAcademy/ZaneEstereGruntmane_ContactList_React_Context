26249fdca4d4add8e38dd7fc98a83a5f142a67be
Zane Estere Gruntmane <141255754+esterzane@users.noreply.github.com>
2023-11-26 20:20:18 +0000

    Contact list Home view structure drafted

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <footer className="footer mt-auto py-3 text-center">
+    <p> Created by
+      <i className="fa-solid fa-otter fa-spin ms-1" style={{ color: "#d4e212" }}></i> coloured by{" "}
+       
+      <i className="fa-solid fa-fish fa-fade ms-1" style={{ color: "#16c0b5" }}></i>
+    </p>
+  </footer>
 );
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,23 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="container-fluid text-center">
+		<div className="cards-wrapper">
+			<div className="card-body border border-primary-subtle ms-5 me-5 row g-2 g-lg-3">
+				<img
+					src={rigoImage}
+					className="Foto rounded-circle col-2 col-lg-2"
+					alt="..."
+				/>
+				<div className="contactInfo col-6 col-lg-6"> This is some text within a card body.</div>
+				<div className="contactChange-wrapper col-4 col-lg-4 text-center">
+					<i className="contactToEdit fa-regular fa-pen-to-square" style={{ color: "#414a6c" }}></i>
+
+					<i className="contactToDelete fa-solid fa-trash-arrow-up" style={{ color: "#414a6c" }}></i>
+
+				</div>
+
+			</div>
+		</div>
 	</div>
 );
