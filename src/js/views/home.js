3ef142b1abd4c99726c1fb9b68d638863f1fa722
Zane Estere Gruntmane <141255754+esterzane@users.noreply.github.com>
2023-11-27 19:02:58 +0000

    contact list template bootstrap updated

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,22 +4,39 @@ import "../../styles/home.css";
 
 export const Home = () => (
 	<div className="container-fluid text-center">
-		<div className="cards-wrapper">
+		<div className="card-wrapper">
 			<div className="card-body border border-primary-subtle ms-5 me-5 row g-2 g-lg-3">
 				<img
 					src={rigoImage}
-					className="Foto rounded-circle col-2 col-lg-2"
+					className="photo rounded-circle col-2 col-lg-2"
 					alt="..."
 				/>
-				<div className="contactInfo col-6 col-lg-6"> This is some text within a card body.</div>
-				<div className="contactChange-wrapper col-4 col-lg-4 text-center">
-					<i className="contactToEdit fa-regular fa-pen-to-square" style={{ color: "#414a6c" }}></i>
+				<div className="cardtInfo col-6 col-lg-6">
+					<ol className="list-group">
+						<li className="list-group-item d-flex justify-content-between align-items-start border border-0">
+							<div className="fw-bold fs-4"> Name
+								<div className="address">
+									<i className="fa-sharp fa-solid fa-location-pin" style={{ color: "#c5d7f6" }}></i>
+								</div>
+								<div className="phone">
+									<i className="fa-sharp fa-solid fa-phone" style={{ color: "#c5d7f6" }} ></i>
+								</div>
+								<div className="email">
+									<i className="fa-sharp fa-regular fa-envelope" style={{ color: "#c5d7f6" }}  ></i>
+								</div>
+							</div>
+						</li>
+					</ol>
+				</div>
 
-					<i className="contactToDelete fa-solid fa-trash-arrow-up" style={{ color: "#414a6c" }}></i>
+				<div className="contactChange-wrapper col-4 col-lg-4 align-self-center">
+					<i className="contactToEdit fa-regular fa-pen-to-square fa-xl" style={{ color: "#414a6c" }}></i>
+
+					<i className="contactToDelete fa-solid fa-trash-arrow-up fa-xl" style={{ color: "#414a6c" }}></i>
 
 				</div>
 
-			</div>
-		</div>
-	</div>
+			</div >
+		</div >
+	</div >
 );
