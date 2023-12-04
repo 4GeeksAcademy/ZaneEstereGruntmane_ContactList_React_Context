5474308151a51c502389ae1a2036de0ae8633016
Zane Estere Gruntmane <141255754+esterzane@users.noreply.github.com>
2023-12-04 06:43:36 +0000

    formated form and css but the funcionality for delete and edit buttons are not working

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -21,7 +21,7 @@ export const Home = () => {
 		<div className="container-fluid text-center">
 			{store.contacts && store.contacts.map(el =>
 				<div className="card-wrapper mb-3">
-					<div className="card-body border border-primary-subtle row ms-5 me-5  g-2 g-lg-2">
+					<div className="card-body border border-primary-subtle rounded row ms-5 me-5  g-2 g-lg-2">
 						<img
 							src={rigoImage}
 							className="photo rounded-circle col-2 col-lg-2"
@@ -30,7 +30,7 @@ export const Home = () => {
 						<div className="card-info col-8 col-lg-6">
 							<ol className="list-group">
 								<li className="list-group-item d-flex justify-content-between align-items-start border border-0">
-									<div className="justify-content-start">
+									<div className="justify-content-start ms-3">
 										<div className="fw-bold fs-4"> {el.full_name} </div>
 										<div className="address">
 											<i className="fa-sharp fa-solid fa-location-pin me-2" style={{ color: "#c5d7f6" }}></i>
